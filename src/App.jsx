4980d1a2990466aae7a980834807b64124eda27a
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-12 15:06:18 +0500

    useActionState end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import React, {useState} from 'react'
+import React, {useActionState} from 'react'
 import {updateNameInDb} from "./api.js";
 
 const App = () => {
-    const [name,setName] = useState(()=>JSON.parse(localStorage.getItem("name")) || "Anonymous name");
-    const [loading,setLoading] = useState(false);
-    const [error,setError] = useState(null);
+    const [name, actionFunction, isPending] = useActionState(formAction, JSON.parse(localStorage.getItem("name")) || "Anonymous user");
 
-    async function handleSubmit(event){
-        event.preventDefault();
-        setLoading(true);
-        setError(null);
-        try{
-            const newName = await updateNameInDb(event.target.name.value);
-            setName(newName);
-        }catch(error){
-            setError(error.message)
-        }finally{
-            setLoading(false);
+    async function formAction(prevState, formAction) {
+        try {
+            return await updateNameInDb(formAction.get("name"))
+        } catch (error) {
+            console.error(error.message)
         }
     }
+
     return (
         <>
-            <p className='username'>
-                Current user: <span>{name}</span>
+            <p>
+                Current user:<span>{name}</span>
             </p>
-            <form onSubmit={handleSubmit}>
-                <input type='text' name={'name'} required/>
-                <button type='submit' disabled={loading}>Submit</button>
+            {isPending && <p>Loading..</p>}
+            <form action={actionFunction}>
+                <input
+                    type='text'
+                    name={'name'}
+                    required/>
+                <button type='submit'>Submit</button>
             </form>
-            {error && <p>Error: {error}</p>}
-            {loading && <p>Loading..</p>}
         </>
     )
 }
