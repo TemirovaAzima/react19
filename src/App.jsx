b05b90871100a3de0159f2fa769a8e544bc24f1d
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-12 15:26:00 +0500

    in Clean code u cant use the same variable name if  their functionality differ in function

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,29 @@ import React, {useActionState} from 'react'
 import {updateNameInDb} from "./api.js";
 
 const App = () => {
-    const [name, actionFunction, isPending] = useActionState(formAction, JSON.parse(localStorage.getItem("name")) || "Anonymous user");
+    const [name,actionFunction,isPending] = useActionState(updateName,JSON.parse(localStorage.getItem("name")) || "Anonymous user");
 
-    async function formAction(prevState, formAction) {
-        try {
+    async function updateName(prevState,formAction){
+        try{
             return await updateNameInDb(formAction.get("name"))
-        } catch (error) {
+        }catch(error){
             console.error(error.message)
         }
     }
-
     return (
-        <>
-            <p>
-                Current user:<span>{name}</span>
-            </p>
-            {isPending && <p>Loading..</p>}
-            <form action={actionFunction}>
-                <input
-                    type='text'
-                    name={'name'}
-                    required/>
-                <button type='submit'>Submit</button>
-            </form>
-        </>
+          <>
+              <p className={'userName'}>
+                  Current user:<span>{name}</span>
+              </p>
+              {isPending && <p>Loading..</p>}
+              <form action={actionFunction}>
+                  <input
+                  type='text'
+                  name={'name'}
+                  required/>
+                  <button type='submit'>Submit</button>
+              </form>
+          </>
     )
 }
 export default App
