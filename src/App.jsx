3c3f5bf81af57aecec957d45ccecdea7eec8eb1d
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-12 14:10:34 +0500

    form with action and with error & loading states

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import {updateNameInDb} from "./api.js";
 
 const App = () => {
     const [name, setName] = useState(
-        () => JSON.parse(localStorage.getItem("name") || "Anonymous user")
-    )
+        () => JSON.parse(localStorage.getItem("name")) || "Anonymous user");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     async function handleSubmit(formAction) {
+        setLoading(true);
+        setError(null);
         try {
-            const newName = await updateNameInDb(formAction.get("name"));
-            setName(newName)
+           const newName = await updateNameInDb(formAction.get("name"));
+           setName(newName);
         } catch (error) {
-            console.error(error.message)
+            console.error(error.message);
+            setError(error);
+        }finally{
+            setLoading(false);
         }
     }
 
