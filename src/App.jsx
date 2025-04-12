207abc65d8799268ed364f6add70d8035f386062
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-12 14:38:22 +0500

    form with onSubmit and with error & loading states

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,34 @@ import React, {useState} from 'react'
 import {updateNameInDb} from "./api.js";
 
 const App = () => {
-    const [name, setName] = useState(
-        () => JSON.parse(localStorage.getItem("name")) || "Anonymous user");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [name,setName] = useState(()=>JSON.parse(localStorage.getItem("name")) || "Anonymous name");
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState(null);
 
-    async function handleSubmit(formAction) {
+    async function handleSubmit(event){
+        event.preventDefault();
         setLoading(true);
         setError(null);
-        try {
-           const newName = await updateNameInDb(formAction.get("name"));
-           setName(newName);
-        } catch (error) {
-            console.error(error.message);
-            setError(error);
+        try{
+            const newName = await updateNameInDb(event.target.name.value);
+            setName(newName);
+        }catch(error){
+            setError(error.message)
         }finally{
             setLoading(false);
         }
     }
-
     return (
         <>
-            <p className={'username'}>
+            <p className='username'>
                 Current user: <span>{name}</span>
             </p>
-            <form action={handleSubmit}>
-                <input
-                    type="text"
-                    name={"name"}
-                    required
-                />
-                <button type={'submit'}>Update</button>
+            <form onSubmit={handleSubmit}>
+                <input type='text' name={'name'} required/>
+                <button type='submit' disabled={loading}>Submit</button>
             </form>
+            {error && <p>Error: {error}</p>}
+            {loading && <p>Loading..</p>}
         </>
     )
 }
